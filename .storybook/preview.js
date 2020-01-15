9c36b676f18dd6b2c72ab6418a7bcef9f4301294
rijkvanzanten <rijkvanzanten@me.com>
2020-01-15 13:10:48 -0500

    Attempt to configure storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+
+import "../src/design/_fonts.scss";
+import "../src/design/_variables.scss";
+
+Vue.use(VueCompositionAPI);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,59 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const path = require('path');
 
-module.exports = async ({ config, mode }) => {
-  config.module.rules.push({
-    test: /\.md$/,
-    use: [
-      {
-        loader: "html-loader"
-      },
-      {
-        loader: "markdown-loader"
-      }
-    ],
-  });
-  
-  return config;
+module.exports = async ({ config }) => {
+	config.resolve = {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			vue$: 'vue/dist/vue.esm.js',
+			'@': path.join(__dirname, '../src')
+		}
+	};
+
+	config.module.rules.push({
+		test: /\.stories\.ts?$/,
+		exclude: /node_modules/,
+		use: [
+			{
+				loader: require.resolve('babel-loader')
+			}
+		]
+	});
+
+	config.module.rules.push({
+		test: /\.scss$/,
+		use: [
+			'vue-style-loader',
+			'css-loader',
+			'sass-loader'
+		],
+	});
+
+	config.module.rules.push({
+		test: /\.ts$/,
+		exclude: /node_modules/,
+		use: [
+			{
+				loader: 'ts-loader',
+				options: {
+					appendTsSuffixTo: [/\.vue$/],
+					transpileOnly: true
+				},
+			}
+		]
+	});
+
+	config.module.rules.push({
+		test: /\.svg$/,
+		exclude: /node_modules/,
+		use: [
+			{
+				loader: "html-loader"
+			}
+		]
+	})
+
+	config.plugins.push(new ForkTsCheckerWebpackPlugin());
+
+	return config;
 };
